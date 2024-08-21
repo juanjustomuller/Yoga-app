d29a149cee901148e04c7256ec87fa62e4ec7b60
JuanJustoMuller <juancitomuller@gmail.com>
2024-08-21 17:08:32 -0300

    creacion de cart routes, y de enrolament routes

--- backend/index.js
+++ backend/index.js
@@ -35,13 +35,16 @@ async function run() {
     const enrolledCollections = database.collection("enrolled");
     const applideCollections = database.collection("applide");
 
-    //class routes
+    //----------------------------------CLASSES ROUTES --------------------------------------------------
+
+    //crear clase
     app.post("/new-class", async (req, res) => {
       const newClass = req.body;
       const result = await classesCollections.insertOne(newClass);
       res.send(result);
     });
 
+    //get all classes
     app.get("/classes", async (req, res) => {
       const query = { status: "aprobado" };
       const result = await classesCollections.find().toArray();
@@ -118,6 +121,142 @@ async function run() {
       const result = await classesCollections.updateOne(filter, updateDoc, options)
       res.send(result)
     })
+
+
+    //------------------------------------------CART ROUTES------------------------------------------------------
+
+    //create a cart item
+    app.post('/add-to-cart', async (req, res) => {
+      const newCartItem = req.body;
+      const result = await cartCollections.insertOne(newCartItem)
+      res.send(result)
+    })
+
+    //get cart item by ID
+    app.get('/cart-item/:id', async (req, res) => {
+      const id = req.params.id;
+      const email = req.body.email;
+      const query = {
+        classId: id,
+        userMail: email
+      };
+      const projection = { classId: 1}; //le dice a la BD q campos especificos queres que te devuelva, en este caso me dara solo el campo classId
+      const result = await cartCollections.findOne(query, {projection: projection});
+      res.send(result)
+    })
+
+    //get cart info by user email
+    app.get('/cart/:email', async (req, res) => {
+      const email = req.params.email
+      const query = {userMail: email}
+      const projection = {classId: 1}
+      const carts = await cartCollections.find(query, {projection: projection});
+      const classIds = carts.map((cart) => new ObjectId(cart.classId))
+      const query2 = {_id: {$in: classIds}}
+      const result = await classesCollections.find(query2).toArray()
+      res.send(result)
+    })
+
+    //------------------------------------------ ENROLAMENT ROUTES (RUTAS DE INSCRIPCION)---------------------------
+
+    //get classes list
+    app.get('/popular_classes', async (req, res) => {
+      const result = await classesCollections.find().sort({totalEnrolled: -1}).limit(6).toArray()
+      //totalEnrolled es un campo en cada clase que indica cuántas personas se han inscrito en ella.
+      //-1 significa que las clases se ordenan de mayor a menor, es decir, de la más popular a la menos popular.
+      //.limit(6) significa que solo se devolverán las 6 clases más populares.
+      res.send(result)
+    })
+
+    //get popular instructors
+
+    /*PIPELINE: Te permite procesar datos en varias etapas, donde cada etapa realiza una operación específica, 
+    y el resultado de una etapa se convierte en la entrada de la siguiente.*/
+    app.get('/popular_instructors', async (req, res) => {
+      const pipeline = [
+        {
+          $group: {
+            _id: "$instructorEmail",  //Agrupa las clases por el correo electrónico del instructor
+            totalEnrolled: {$sum: "$totalEnrolled"}  //suma el número total de estudiantes inscritos
+          }
+        },
+        {
+          $lookup: {   //búsqueda(lookup) en otra colección llamada users, 
+            from: "users",    //para encontrar el documento del instructor correspondiente a cada correo electrónico
+            localField: "_id",
+            foreignField: "email",
+            as: "instructor"   //Añade un campo llamado instructor que contiene la información del usuario (el instructor) cuyo correo electrónico coincide con _id.
+          }
+        },
+        {
+          $project: {   //Selecciona (proyecta) solo algunos campos del resultado anterior
+            _id: 0,     //Elimina _id (lo pone en 0, lo que significa que no se mostrará).
+            instructor: {
+              $arrayElemAt: ["instructor", 0]
+            },
+            totalEnrolled: 1    //Para cada instructor, tendrás solo su información básica (el primer elemento en el array instructor) 
+          }                     //y el total de estudiantes inscritos.
+        },
+        {
+          $sort: {  //Ordena los instructores por totalEnrolled en orden descendente (de mayor a menor).
+            totalEnrolled: -1   //Los instructores con más estudiantes inscritos aparecerán primero.
+          }
+        },
+        {
+          $limit: 6   //Limita el resultado a 6 instructores. Solo verás los 6 instructores más populares.
+        }
+      ];
+
+      const result = await classesCollections.aggregate(pipeline).toArray()
+      res.send(result)
+    })
+
+    //admin status
+    app.get('admin-status', async (req, res) => {
+      const approvedClasses = ((await classesCollections.find({status: "aprobado"})).toArray()).length; // tendrá un número que indica la cantidad de clases aprobadas
+      const pendingClasses =  ((await classesCollections.find({status: "Pendiente"})).toArray()).length; //contendrá la cantidad de clases pendientes de aprobación.
+      const instructors =  ((await userCollections.find({role: "instructor"})).toArray()).length; //endrá la cantidad total de instructores registrados
+      const totalClasses =  (await classesCollections.find().toArray()).length; //contendrá el número total de clases
+      const totalEnrolled = (await enrolledCollections.find().toArray()).length; //tendrá el número total de inscripciones de estudiantes.
+
+      const result = {
+        approvedClasses,
+        pendingClasses,
+        instructors,
+        totalClasses,
+        totalEnrolled
+      }
+
+      res.send(result)
+
+    })
+
+
+
+
+
+
+
+
+
+    //delete cart item
+    app.delete('/delete-cart-item/:id', async (req, res) => {
+      const id = req.params.id
+      const query = { classId: id}
+      const result = await cartCollections.deleteOne(query)
+      res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
